e8abfd4ea7c7006fd87bce547fa9a8ba390e86e3
saralazicc <sara.lazic@ctd.ai>
2023-07-13 22:37:57 +0200

    set up localization!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SvgIconComponent } from './components/svg-icon/svg-icon.component';
 import { SpecialFunctionComponent } from './components/special-function/special-function.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { AboutComponent } from './components/about/about.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { AboutComponent } from './components/about/about.component';
     HomepageComponent,
     AboutComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  greeting?: string;
+  message?: string;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadTranslations('en.json'); // Load English translations by default
+  }
+
+  loadTranslations(lang: string) {
+    this.http.get(`./assets/i18n/${lang}`).subscribe((translations: any) => {
+      this.greeting = translations.greeting;
+      this.message = translations.message;
+    });
+  }
 }
