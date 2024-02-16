870677710ac6ae69b1ca1438b5b9cf4d2bc16729
saralazicc <sara.lazic@ctd.ai>
2024-02-17 00:25:55 +0100

    function information added

--- src/app/components/function-information/function-information.component.ts
+++ src/app/components/function-information/function-information.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { FunctionType } from 'src/app/models/enums';
 import { SpecialFunction } from 'src/app/services/functions/specialFunction';
 import { LanguageService } from 'src/app/services/language-service/language.service';
 import { createChosenFunction } from 'src/utilities/utilities';
@@ -21,6 +22,19 @@ export class FunctionInformationComponent {
   relationsUrl: string[] = [];
   equationUrl: string = '';
 
+  styleDef: number = 0;
+  styleDomain: number = 0;
+  styleRel: number = 0;
+  styleEqu: number = 0;
+
+  photoStyle: number = 0;
+
+  // translations
+  definition = '';
+  domain = '';
+  equation = '';
+  relations = '';
+
   constructor(
     private route: ActivatedRoute,
     private http: HttpClient,
@@ -31,6 +45,7 @@ export class FunctionInformationComponent {
     this.parameter = this.route.snapshot.paramMap.get('parameter');
     this.spef = createChosenFunction(this.parameter ?? '');
     this.generatePhotoUrls();
+    this.loadStyles();
     this.loadTranslations();
 
     this.subscription = this.languageService
@@ -46,14 +61,16 @@ export class FunctionInformationComponent {
 
   generatePhotoUrls() {
     const baseUrl = '../../../assets/functions';
-    const definitionsCnt = this.parameter === 'hermite2' ? 1 : 3;
+    const definitionsCnt =
+      this.parameter === FunctionType.HERMITE_PROBABILISTIC ? 1 : 3;
     for (let i = 1; i <= definitionsCnt; i++) {
       this.definitionUrls.push(
         `${baseUrl}/definitions/${this.parameter}_${i}.png`
       );
     }
 
-    const relationsCnt = this.parameter === 'hermite2' ? 2 : 4;
+    const relationsCnt =
+      this.parameter === FunctionType.HERMITE_PROBABILISTIC ? 2 : 4;
     for (let i = 1; i <= relationsCnt; i++) {
       this.relationsUrl.push(`${baseUrl}/relations/${this.parameter}_${i}.png`);
     }
@@ -71,6 +88,86 @@ export class FunctionInformationComponent {
       .get(`./assets/i18n/${selectedLanguage}.json`)
       .subscribe((translations: any) => {
         this.spef?.loadTranslations(translations);
+        this.definition = translations.function_information.definition;
+        this.domain = translations.function_information.domain;
+        this.equation = translations.function_information.equation;
+        this.relations = translations.function_information.relations;
       });
+  }
+
+  loadStyles() {
+    switch (this.parameter) {
+      case FunctionType.BESSEL_FIRST_KIND: {
+        this.styleDef = 50;
+        this.styleDomain = 50;
+        this.styleRel = 50;
+        this.styleEqu = 50;
+        this.photoStyle = 70;
+        break;
+      }
+      case FunctionType.HERMITE_PHYSICIST: {
+        this.styleDef = 55;
+        this.styleDomain = 57;
+        this.styleRel = 55;
+        this.styleEqu = 60;
+        this.photoStyle = 90;
+        break;
+      }
+      case FunctionType.HERMITE_PROBABILISTIC: {
+        this.styleDef = 60;
+        this.styleDomain = 64;
+        this.styleRel = 65;
+        this.styleEqu = 60;
+        this.photoStyle = 90;
+        break;
+      }
+      case FunctionType.LAGUERRE_POLYNOMIAL: {
+        this.styleDef = 55;
+        this.styleDomain = 60;
+        this.styleRel = 57;
+        this.styleEqu = 60;
+        this.photoStyle = 90;
+        break;
+      }
+      case FunctionType.LEGENDRE_POLYNOMIAL: {
+        this.styleDef = 55;
+        this.styleDomain = 55;
+        this.styleRel = 60;
+        this.styleEqu = 55;
+        this.photoStyle = 90;
+        break;
+      }
+      case FunctionType.CHEBYSHEV_FIRST_KIND: {
+        this.styleDef = 60;
+        this.styleDomain = 55;
+        this.styleRel = 60;
+        this.styleEqu = 60;
+        this.photoStyle = 90;
+        break;
+      }
+      case FunctionType.CHEBYSHEV_SECOND_KIND: {
+        this.styleDef = 60;
+        this.styleDomain = 55;
+        this.styleRel = 60;
+        this.styleEqu = 60;
+        this.photoStyle = 90;
+        break;
+      }
+      case FunctionType.JACOBI_POLYNOMIAL: {
+        this.styleDef = 60;
+        this.styleDomain = 60;
+        this.styleRel = 65;
+        this.styleEqu = 65;
+        this.photoStyle = 90;
+        break;
+      }
+      default: {
+        this.styleDef = 25;
+        this.styleDomain = 15;
+        this.styleRel = 25;
+        this.styleEqu = 25;
+        this.photoStyle = 80;
+      }
+    }
   }
 }
